4ef856d0628455206d7156fcd0289e03e1214716
Ivica Tokic <Tokic@ivicas-mac-mini.cm\.bnet\.lan>
2017-11-13 19:39:12 +0100

    Initial Commit

---
+++ ZagrebMuseums WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ZagrebMuseums WatchKit Extension
+//
+//  Created by Ivica Tokic on 13/11/2017.
+//  Copyright © 2017 Ivica Tokic. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
